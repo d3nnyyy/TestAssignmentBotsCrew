7e368346816d533b3d5a1cfd5a66d5ff31607f34
d3nnyyy <d3nnn41k@gmail.com>
2023-11-03 22:45:20 +0200

    refactored entities to contain validation and added set of lectors to department

--- src/main/java/ua/dtsebulia/testassignmentbotscrew/entity/Department.java
+++ src/main/java/ua/dtsebulia/testassignmentbotscrew/entity/Department.java
@@ -1,10 +1,13 @@
 package ua.dtsebulia.testassignmentbotscrew.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Set;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,8 +20,12 @@ public class Department {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotNull
     private String name;
 
     @ManyToOne
     private Lector headOfDepartment;
+
+    @ManyToMany
+    private Set<Lector> lectors;
 }
--- src/main/java/ua/dtsebulia/testassignmentbotscrew/entity/Lector.java
+++ src/main/java/ua/dtsebulia/testassignmentbotscrew/entity/Lector.java
@@ -1,6 +1,7 @@
 package ua.dtsebulia.testassignmentbotscrew.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,8 +21,12 @@ public class Lector {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotNull
     private String firstName;
+    @NotNull
     private String lastName;
+    @NotNull
+    private Integer salary;
 
     @ManyToOne
     private Degree degree;
