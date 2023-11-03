c29f074317bd332fec3b342a6a07332fe28a1c81
d3nnyyy <d3nnn41k@gmail.com>
2023-11-03 21:13:08 +0200

    initial commit

---
+++ src/main/java/ua/dtsebulia/testassignmentbotscrew/TestAssignmentBotsCrewApplication.java
@@ -0,0 +1,13 @@
+package ua.dtsebulia.testassignmentbotscrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAssignmentBotsCrewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAssignmentBotsCrewApplication.class, args);
+    }
+
+}
