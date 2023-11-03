8e0ec76a736e4bd91edb430b232f1ad202adfe58
d3nnyyy <d3nnn41k@gmail.com>
2023-11-03 22:57:46 +0200

    added get full name method

--- src/main/java/ua/dtsebulia/testassignmentbotscrew/entity/Lector.java
+++ src/main/java/ua/dtsebulia/testassignmentbotscrew/entity/Lector.java
@@ -33,4 +33,8 @@ public class Lector {
 
     @OneToMany(mappedBy = "headOfDepartment")
     private Set<Department> departments;
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
 }
