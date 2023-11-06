3b9736a12b4fbac1d8615702a14283cf33f5cbad
d3nnyyy <d3nnn41k@gmail.com>
2023-11-06 10:24:35 +0200

    tested get department statistics method

--- src/test/java/ua/dtsebulia/testassignmentbotscrew/service/DepartmentServiceTest.java
+++ src/test/java/ua/dtsebulia/testassignmentbotscrew/service/DepartmentServiceTest.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import ua.dtsebulia.testassignmentbotscrew.entity.Degree;
 import ua.dtsebulia.testassignmentbotscrew.entity.Department;
 import ua.dtsebulia.testassignmentbotscrew.entity.Lector;
 import ua.dtsebulia.testassignmentbotscrew.repository.DepartmentRepository;
+
+import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
@@ -66,6 +70,75 @@ class DepartmentServiceTest {
 
         String result = departmentService.findHeadOfDepartment("Test Department");
         assertEquals("Department with name Test Department not found.", result);
+    }
+
+    @Test
+    void testGetDepartmentStatistic() {
+
+        Degree assistantDegree = Degree
+                .builder()
+                .name("assistant")
+                .build();
+
+        Degree associateProfessorDegree = Degree
+                .builder()
+                .name("associate professor")
+                .build();
+
+        Degree professorDegree = Degree
+                .builder()
+                .name("professor")
+                .build();
+
+        Lector assistant = Lector
+                .builder()
+                .degree(assistantDegree)
+                .build();
+
+        Lector associateProfessor = Lector
+                .builder()
+                .degree(associateProfessorDegree)
+                .build();
+
+        Lector professor = Lector
+                .builder()
+                .degree(professorDegree)
+                .build();
+
+        Department department = Department
+                .builder()
+                .name("Test Department")
+                .lectors(Set.of(assistant, associateProfessor, professor))
+                .build();
+
+
+        when(departmentRepository.findByName("Test Department")).thenReturn(department);
+
+        String result = departmentService.getDepartmentStatistic("Test Department");
+        assertEquals("assistants - 1.\nassociate professors - 1.\nprofessors - 1.", result);
+    }
+
+    @Test
+    void testGetDepartmentStatisticWhenDepartmentNotFound() {
+
+        when(departmentRepository.findByName("Test Department")).thenReturn(null);
+
+        String result = departmentService.getDepartmentStatistic("Test Department");
+        assertEquals("Department with name Test Department not found.", result);
+    }
+
+    @Test
+    void testGetDepartmentStatisticWhenNoLectors() {
+
+        Department department = Department
+                .builder()
+                .name("Test Department")
+                .build();
+
+        when(departmentRepository.findByName("Test Department")).thenReturn(department);
+
+        String result = departmentService.getDepartmentStatistic("Test Department");
+        assertEquals("Department Test Department has no lectors.", result);
     }
 
 }
